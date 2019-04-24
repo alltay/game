1a43f0b7b612469c3cfecb3ea2e037edb279bbd6
alltay <alltay@ya.ru>
2019-04-24 17:59:00 +0300

    iland fix

--- js/functions/checkClickIlandAccept.js
+++ js/functions/checkClickIlandAccept.js
@@ -5,7 +5,7 @@ export default function checkClickIlandAccept(map, e) {
     if(map["type"] == 1 && e.offsetX < map["x"] + map["w"] - 10 && e.offsetX > map["x"] + 30 && e.offsetY > map["y"] + 30  && e.offsetY < map["y"] + map["h"] - 10) {  // УТОЧНИТЬ !!
 
       $('#myModal').modal('show');
-      iland_modal.innerText = map["way"]
+      iland_modal.innerText = map["info"]
 
     } 
 }
